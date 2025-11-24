4a46895b6a4d1f796680831935bf878d48406f15
Ran Levy <Ran.levy02@post.runi.ac.il>
2025-11-24 23:10:59 +0200

    gf

--- MyString.java
+++ MyString.java
@@ -17,35 +17,40 @@ public class MyString {
     }
 
     /** Returns the lowercase version of the given string. */
-    public static String lowerCase(String str) {
-        String newww = "";
-       for(int i=0;i<str.length();i++){
+   public static String lowerCase(String str) {
+    String newww = "";
+    for(int i=0;i<str.length();i++){
         if(str.charAt(i)>='A' && str.charAt(i)<='Z')
-            newww= newww + (char)(str.charAt(i)+32);
+            newww = newww + (char)(str.charAt(i)+32);
         else
-            newww= newww + str.charAt(i);
-       }
-        return newww;
+            newww = newww + str.charAt(i);
     }
+    return newww;
+}
 
-    /** If str1 contains str2, returns true; otherwise returns false. */
-    public static boolean contains(String str1, String str2) {
-        int count =0;
-            for(int i = 0; i<str1.length(); i++){
-                if(str1.charAt(i)==str2.charAt(0)){
-                    for(int j = 1 ; j<str2.length();j++){
-                   if (i + j >= str1.length() || str1.charAt(i + j) != str2.charAt(j)) {
+public static boolean contains(String str1, String str2) {
+
+    if (str2.length() == 0)
+        return true;
+
+    int count = 0;
+
+    for(int i = 0; i < str1.length(); i++){
+        if(str1.charAt(i) == str2.charAt(0)){
+            for(int j = 1; j < str2.length(); j++){
+                if (i + j >= str1.length() || str1.charAt(i + j) != str2.charAt(j)) {
                     count = 0;
                     break;
                 }
-                        if(str1.charAt(i+j)==str2.charAt(j))
-                            count++;
-                    }
-                    if(count+1==str2.length())
-                        return true; 
-                }
-                 count =0;
+                if(str1.charAt(i + j) == str2.charAt(j))
+                    count++;
             }
-        return false;
+            if(count + 1 == str2.length())
+                return true; 
+        }
+        count = 0;
     }
+
+    return false;
+}
 }
--- Primes.java
+++ Primes.java
@@ -1,42 +1,42 @@
 public class Primes {
     public static void main(String[] args) {
-      int n = Integer.parseInt(args[0]);
-        boolean[] prime = new boolean[n + 1];
+    int n = Integer.parseInt(args[0]);
+    boolean[] prime = new boolean[n + 1];
 
-        int i = 2;
-        while (i <= n) {
-            prime[i] = true;
-            i++;
-        }
-
-        i = 2;
-        while (i * i <= n) {
-            if (prime[i]) {
-                int k = i * i;
-                while (k <= n) {
-                    prime[k] = false;
-                    k += i;
-                }
-            }
-            i++;
-        }
-
-        System.out.println("Prime numbers up to " + n + ":");
-
-        int count = 0;
-        i = 2;
-
-        while (i <= n) {
-            if (prime[i]) {
-                System.out.println(i);
-                count++;
-            }
-            i++;
-        }
-
-        double percent = (100.0 * count) / (n - 1);
-
-        System.out.println("There are " + count + " primes between 2 and " + n +
-                           " (" + percent + "% are primes)");
+    int i = 2;
+    while (i <= n) {
+        prime[i] = true;
+        i++;
     }
+
+    i = 2;
+    while (i * i <= n) {
+        if (prime[i]) {
+            int k = i * i;
+            while (k <= n) {
+                prime[k] = false;
+                k += i;
+            }
+        }
+        i++;
+    }
+
+    System.out.println("Prime numbers up to " + n + ":");
+
+    int count = 0;
+    i = 2;
+
+    while (i <= n) {
+        if (prime[i]) {
+            System.out.println(i);
+            count++;
+        }
+        i++;
+    }
+
+    double percent = (100.0 * count) / n;
+
+    System.out.println("There are " + count + " primes between 2 and " + n +
+                       " (" + percent + "% are primes)");
+}
 }
